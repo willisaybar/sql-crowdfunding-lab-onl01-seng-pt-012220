22fc3023be1e9b80f61c88e89ad55bbd9a69686c
Willis Aybar <willisaybar@gmail.com>
2020-04-01 23:51:45 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -92,7 +92,7 @@ end
 #group by name
 #order by names SUM
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
-  "SELECT users.name, pledges.amount
+  "SELECT users.name, SUM(pledges.amount)
     FROM users
     JOIN pledges
     ON pledges.user_id = users.id
