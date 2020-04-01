4e050c2f60349c49c366976aa5b9715f2d1adf45
Willis Aybar <willisaybar@gmail.com>
2020-04-01 23:28:39 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -84,7 +84,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
       LEFT OUTER JOIN pledges
       ON projects.id = pledges.project_id
       GROUP BY pledges.amount
-      HAVING pledges.amount <= projects.funding_goal"
+      HAVING pledges.amount >= projects.funding_goal"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
