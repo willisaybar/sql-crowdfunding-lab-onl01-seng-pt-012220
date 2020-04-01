793016bd69b943f180d2755b0124cfb084121446
Willis Aybar <willisaybar@gmail.com>
2020-04-01 23:34:36 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -80,11 +80,12 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
   #   GROUP BY pledges.amount
   #   HAVING funding_goal <= pledges.amount"
     "SELECT Projects.title, (projects.funding_goal - SUM(pledges.amount))
+      AS amount_left
       FROM projects
       JOIN pledges
       ON projects.id = pledges.project_id
       GROUP BY projects.title
-      HAVING pledges.amount <= 0"
+      HAVING amount_left <= 0"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
