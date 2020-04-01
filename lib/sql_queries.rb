c87f4a68e9b6d1971201de23f0b15e0688a2c5b2
Willis Aybar <willisaybar@gmail.com>
2020-04-01 23:57:20 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -104,7 +104,7 @@ end
 #select pledge amounts
 #of all pledges in music category
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-  "SELECT * projects.category, pledges.amount
+  "SELECT projects.category, pledges.amount
     FROM projects
     JOIN pledges
     ON pledges.project_id = projects.id
