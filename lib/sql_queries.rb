4ff3caff475d3a74dfa497a5490ae2ef25329abf
Willis Aybar <willisaybar@gmail.com>
2020-04-02 00:01:13 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -115,7 +115,7 @@ end
 #sum total of all pledges
 #for the books category
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-  "SELECT projects.category, pledges.amount
+  "SELECT projects.category, SUM(pledges.amount)
     FROM projects
     JOIN pledges
     ON pledges.project_id = projects.id
